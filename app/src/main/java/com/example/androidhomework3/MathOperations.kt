bd4f779c8a434b3bb6205a9003ec483acd3af6cb
informant <artemiy.solodkin@mail.ru>
2024-09-30 13:43:59 +0500

    last updates2

--- app/src/main/java/com/example/androidhomework3/MathOperations.kt
+++ app/src/main/java/com/example/androidhomework3/MathOperations.kt
@@ -1,10 +1,10 @@
 package com.example.androidhomework3
 
-import java.math.BigDecimal
+import kotlin.math.abs
 
 object MathOperations {
-    fun plus(a:Double, b:Double) = a+b
-    fun minus(a:Double, b:Double) = a-b
-    fun divide(a:Double, b:Double) = if (b!=0.0) a/b else 0.0
-    fun multiply(a:Double, b:Double) = a*b
+    fun plus(a: Double, b: Double) = a + b
+    fun minus(a: Double, b: Double) = if (a - b < 0) 0.0 else a - b
+    fun divide(a: Double, b: Double) = if (b != 0.0) a / b else 0.0
+    fun multiply(a: Double, b: Double) = a * b
 }
